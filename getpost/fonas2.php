1e69b1160f4ed964be316fdbd740ffe48a1b5309
A-Kija <42836152+A-Kija@users.noreply.github.com>
2019-12-09 11:00:26 +0200

    wertyui

--- getpost/fonas2.php
+++ getpost/fonas2.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html>
-<body style="background:<?php echo $_GET['color']??'white' ?>;">
+<body style="background:<?= $_GET['color'.$_GET['mygtukas']??'']??'white'?>;">
 
 <h2>Spalvotas Fonas 2</h2>
 
 <form action="">
-  Spalva:<br>
-  <input type="radio" name="color" value="red"> red<br>
-  <input type="radio" name="color" value="blue"> blue<br>
-  <input type="radio" name="color" value="black"> black<br>  
+  Spalv0s 1:<br>
+  <input type="radio" name="color1" value="red"> red<br>
+  <input type="radio" name="color1" value="blue"> blue<br>
+  <input type="radio" name="color1" value="black"> black<br>  
   <br><br>
-  <input type="submit" value="Keisti">
+  <button type="submit" name="mygtukas" value="1">Keisti</button>
+  <br><br>
+  Spalv0s 2:<br>
+  <input type="radio" name="color2" value="green"> green<br>
+  <input type="radio" name="color2" value="pink"> pink<br>
+  <input type="radio" name="color2" value="yellow"> yellow<br>  
+  <br><br>
+
+  <button type="submit" name="mygtukas" value="2">Keisti</button>
+
+
 </form> 
 
 </body>
 </html>
+
+<?php
+
+_dc($_GET);
